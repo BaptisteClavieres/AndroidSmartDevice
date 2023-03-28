7a79d5fb946f399654301daa4b4f59eeb975fe96
clavi <clavieres.baptiste@gmail.com>
2023-03-28 16:48:47 +0200

    version mardi 28-03-2023 16h

--- app/src/main/java/fr/isen/clavieres/androidsmartdevice/DeviceActivity.kt
+++ app/src/main/java/fr/isen/clavieres/androidsmartdevice/DeviceActivity.kt
@@ -1,7 +1,18 @@
 package fr.isen.clavieres.androidsmartdevice
 
+import android.bluetooth.BluetoothGattCallback
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import fr.isen.clavieres.androidsmartdevice.databinding.DeviceActivityBinding
 
 class DeviceActivity: AppCompatActivity() {
 
+    private lateinit var binding: DeviceActivity
+    private var bluetoothGatt: BluetoothGattCallback? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //binding = DeviceActivityBinding.inflate(layoutInflater)
+        //setContentView()
+    }
 }
